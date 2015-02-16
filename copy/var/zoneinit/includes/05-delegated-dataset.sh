bcda693cb7b92ea76c82244c20adfa6e8f094a0d
Thomas Merkel <tm@core.io>
2015-02-16 11:44:11 +0100

    use delegate dataset for storing databases

---
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+UUID=$(mdata-get sdc:uuid)
+DDS=zones/${UUID}/data
+
+if zfs list ${DDS} 1>/dev/null 2>&1; then
+	zfs create ${DDS}/mysql || true
+	zfs set mountpoint=/var/mysql ${DDS}/mysql
+fi
